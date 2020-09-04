ecabc0d4622a1d30b3e7196f8e0f9ca4e0cce66c
fbrito <fabrice.brito@terradue.com>
2020-09-04 07:37:41 +0000

    fixing notebook

---
+++ helpers.py
@@ -0,0 +1,45 @@
+from urllib.parse import urlparse
+import gdal 
+import numpy as np
+
+def get_vsi_url(enclosure, username, api_key):
+    
+    
+    parsed_url = urlparse(enclosure)
+
+    url = '/vsicurl/{}://{}:{}@{}/api{}'.format(list(parsed_url)[0],
+                                            username, 
+                                            api_key, 
+                                            list(parsed_url)[1],
+                                            list(parsed_url)[2])
+    
+    return url 
+
+
+def vsi_download(row, bbox, username, api_key):
+    
+    vsi_url = get_vsi_url(row.enclosure, username, api_key)
+    
+    ulx, uly, lrx, lry = bbox[0], bbox[3], bbox[2], bbox[1] 
+    
+    # load VSI URL in memory
+    output = '/vsimem/subset.tif'
+    
+    ds = gdal.Open(vsi_url)
+    
+    ds = gdal.Translate(destName=output, 
+                        srcDS=ds, 
+                        projWin = [ulx, uly, lrx, lry], 
+                        projWinSRS = 'EPSG:4326',
+                        outputType=gdal.GDT_Float32)
+    ds = None
+    
+    # create a numpy array
+    ds = gdal.Open(output)
+    
+    layers = []
+
+    for i in range(1, ds.RasterCount+1):
+        layers.append(ds.GetRasterBand(i).ReadAsArray())
+
+    return np.dstack(layers)
